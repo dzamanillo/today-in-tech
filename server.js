4738e7f879705161c0daa1c99d53c4390213ad2f
Daniel Zamanillo <dzamanillok@gmail.com>
2021-09-07 16:57:02 -0700

    handlebars added. still need to setup db

--- server.js
+++ server.js
@@ -1,9 +1,18 @@
+const path = require("path");
 const express = require("express");
+const routes = require("./controller/");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+const exphbs = require("express-handlebars");
+const hbs = exphbs.create({});
+
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.listen(PORT, () => console.log("Now listening!"));
