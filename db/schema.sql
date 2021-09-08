b846fe4e4baf5d37838d983b073d3cc366011418
Daniel Zamanillo <dzamanillok@gmail.com>
2021-09-08 10:18:50 -0700

    schema created and database created

---
+++ db/schema.sql
@@ -0,0 +1,3 @@
+DROP DATABASE IF EXISTS today_tech_db;
+
+CREATE DATABASE today_tech_db;
--- server.js
+++ server.js
@@ -15,4 +15,4 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
-app.listen(PORT, () => console.log("Now listening!"));
+app.listen(PORT, () => console.log(`Now listening on localhost:${PORT}`));
