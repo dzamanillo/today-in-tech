1a1742fd24de0885c6c259c43ae807a8d394d316
Daniel Zamanillo <dzamanillok@gmail.com>
2021-09-08 14:57:09 -0700

    server fixed to work with sequelize and first user route written

---
+++ controller/api/user-routes.js
@@ -0,0 +1,19 @@
+const router = require("express").Router();
+const { User, Post, Comment } = require("../../models");
+
+// Get all users
+// /api/users
+router.get("/", (req, res) => {
+	User.findAll({
+		attributes: { exclude: ["password"] },
+	})
+		.then((dbUserData) => {
+			res.json(dbUserData);
+		})
+		.catch((err) => {
+			console.log(err);
+			res.status(500).json(err);
+		});
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,11 +1,26 @@
 const path = require("path");
 const express = require("express");
-const routes = require("./controller/");
+const session = require("express-session");
+const exphbs = require("express-handlebars");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-const exphbs = require("express-handlebars");
+const sequelize = require("./config/connection");
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
+
+const sess = {
+	secret: "Super secret secret",
+	cookie: {},
+	resave: false,
+	saveUninitialized: true,
+	store: new SequelizeStore({
+		db: sequelize,
+	}),
+};
+
+app.use(session(sess));
+
 const hbs = exphbs.create({});
 
 app.engine("handlebars", hbs.engine);
@@ -15,4 +30,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
-app.listen(PORT, () => console.log(`Now listening on localhost:${PORT}`));
+app.use(require("./controller/"));
+
+sequelize.sync({ force: false }).then(() => {
+	app.listen(PORT, () => console.log(`Now listening on port ${PORT}`));
+});
