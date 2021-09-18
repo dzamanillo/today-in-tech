6d62b45344d70bf071e97b6a44af314364d57618
Daniel Zamanillo <dzamanillok@gmail.com>
2021-09-18 15:40:57 -0500

    created login and sign up pages

--- controller/homeRoutes.js
+++ controller/homeRoutes.js
@@ -2,6 +2,20 @@ const router = require("express").Router();
 const sequelize = require("../config/connection");
 const { User, Post, Comment } = require("../models");
 
+router.get("/login", (req, res) => {
+	Post.findAll({}).then((dbPostData) => {
+		const posts = dbPostData.map((post) => post.get({ plain: true }));
+		res.render("login");
+	});
+});
+
+router.get("/signup", (req, res) => {
+	Post.findAll({}).then((dbPostData) => {
+		const posts = dbPostData.map((post) => post.get({ plain: true }));
+		res.render("signup");
+	});
+});
+
 router.get("/", (req, res) => {
 	Post.findAll({}).then((dbPostData) => {
 		const posts = dbPostData.map((post) => post.get({ plain: true }));
